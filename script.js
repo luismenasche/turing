7c0fb773bb1e922e86e6c3ff890c8557fbad2daf
Luis Menasché <93421667+luismenasche@users.noreply.github.com>
2022-03-25 18:48:37 -0300

    Menu button and behavior

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const checkbox = document.getElementById("nav__checkbox");
-window.addEventListener("scroll", () => checkbox.checked = false);
-
-const menu = document.getElementById("menu");
-const menuItems = menu.children;
+const nav = document.getElementById("nav");
+const menuButton = document.getElementById("nav__button");
+const menuItems = document.getElementById("menu").children;
 let delay = 0;
+
 for (let i of menuItems) {
     i.style.transitionDelay = `${delay}s`;
     delay += 0.1;
 }
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+window.addEventListener("scroll", () => {
+    nav.classList.remove("open");
+});
