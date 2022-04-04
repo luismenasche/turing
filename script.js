07bb19793f6c75d8aaebe732383b59cc893862b2
Luis Menasché <93421667+luismenasche@users.noreply.github.com>
2022-04-04 03:24:57 -0300

    Menu adjustment for small screens

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const nav = document.getElementById("nav");
 const menuButton = document.getElementById("nav__button");
-const menuItems = document.getElementById("menu").children;
+const menuItems = Array.from(document.getElementById("menu").children);
+const dropDowns = menuItems.filter(i => i.classList.contains("has-dropdown"));
 let delay = 0;
 
-for (let i of menuItems) {
-    i.style.transitionDelay = `${delay}s`;
-    delay += 0.1;
-}
-
-menuButton.addEventListener("click", () => {
-    nav.classList.toggle("open");
-});
-window.addEventListener("scroll", () => {
+function closeMenu() {
+    dropDowns.forEach(i => i.classList.remove("dropdown"));
     nav.classList.remove("open");
     nav.scrollTo(0, 0);
+}
+
+menuItems.forEach(i => {
+    i.style.transitionDelay = `${delay}s`;
+    delay += 0.1;
 });
-nav.addEventListener("scroll", ev => {
-    ev.stopPropagation();
+
+menuButton.addEventListener("click", () => {
+    if (nav.classList.contains("open"))
+        closeMenu();
+    else
+        nav.classList.add("open");
 });
+dropDowns.forEach(i => {
+    i.addEventListener("click", () => {
+        dropDowns.forEach(j => j.classList.remove("dropdown"));
+        i.classList.add("dropdown");
+    });
+});
+nav.addEventListener("click", ev => {
+    if (ev.target.tagName != "A")
+        ev.stopPropagation();
+});
+window.addEventListener("click", closeMenu);
